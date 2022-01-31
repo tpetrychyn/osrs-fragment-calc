302989ccc6dd36f4529c331b6cf7367e3b6852b2
Taylor Petrychyn <tpetrych@Taylors-MacBook-Pro.local>
2022-01-30 16:09:40 -0800

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css" integrity="sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5" crossOrigin="anonymous"/>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2S5CN6WWDG"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-2S5CN6WWDG');`}}></script>
+          <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css" integrity="sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5" crossOrigin="anonymous" />
           <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/grids-responsive-min.css" />
         </Head>
         <body>
